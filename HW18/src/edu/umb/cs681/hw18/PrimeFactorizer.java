e6648bd7cd97db4eb7f3cead3c3ddaca10cfb42e
Kanthraj S <kanthraj.s@icloud.com>
2022-05-20 23:57:54 -0400

    edit .gitignore

--- HW18/src/edu/umb/cs681/hw18/PrimeFactorizer.java
+++ HW18/src/edu/umb/cs681/hw18/PrimeFactorizer.java
@@ -2,59 +2,72 @@ package edu.umb.cs681.hw18;
 
 import java.util.LinkedList;
 
-
 public class PrimeFactorizer {
 	protected long dividend, from, to;
 	protected LinkedList<Long> factors = new LinkedList<Long>();
-	
-	public PrimeFactorizer(long dividend){
-		if(dividend >= 2){
+
+	public PrimeFactorizer(long dividend) {
+		if (dividend >= 2) {
 			this.dividend = dividend;
 			this.from = 2;
 			this.to = dividend;
-		}else{
+		} else {
 			throw new RuntimeException("dividend must be greater 2. dividend==" + dividend);
 		}
 	}
 
-	public long getDividend() { return dividend; }
-	public long getFrom(){ return from; }
-	public long getTo(){ return to; }
-	public LinkedList<Long> getPrimeFactors(){ return factors; }
-	
+	public long getDividend() {
+		return dividend;
+	}
+
+	public long getFrom() {
+		return from;
+	}
+
+	public long getTo() {
+		return to;
+	}
+
+	public LinkedList<Long> getPrimeFactors() {
+		return factors;
+	}
+
 	public void generatePrimeFactors() {
 		long divisor = 2;
-	    while( dividend != 1 && divisor <= to ){
-		    if(dividend % divisor == 0) {
-		        factors.add(divisor);
-		        dividend /= divisor;
-		    }else {
-		    	if(divisor==2){ divisor++; }
-		    	else{ divisor += 2; }
-		    }
+		while (dividend != 1 && divisor <= to) {
+			if (dividend % divisor == 0) {
+				factors.add(divisor);
+				dividend /= divisor;
+			} else {
+				if (divisor == 2) {
+					divisor++;
+				} else {
+					divisor += 2;
+				}
+			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		System.out.print("Prime factors of 7: ");
 		PrimeFactorizer fac = new PrimeFactorizer(7);
-		fac.generatePrimeFactors();
-		System.out.println(fac.getPrimeFactors());
-		
+		factors1.generatePrimeFactors();
+		System.out.println(factors1.getPrimeFactors());
+
 		System.out.print("Prime factors of 21: ");
-		fac = new PrimeFactorizer(21);
-		fac.generatePrimeFactors();
-		System.out.println(fac.getPrimeFactors());
+		factors1 = new PrimeFactorizer(21);
+		factors1.generatePrimeFactors();
+		System.out.println(factors1.getPrimeFactors());
 
 		System.out.print("Prime factors of 207: ");
-		fac = new PrimeFactorizer(207);
-		fac.generatePrimeFactors();
-		System.out.println(fac.getPrimeFactors());
-		
+		factors1 = new PrimeFactorizer(207);
+		factors1.generatePrimeFactors();
+		System.out.println(factors1.getPrimeFactors());
+
 		System.out.print("Prime factors of 100: ");
-		fac = new PrimeFactorizer(100);
-		fac.generatePrimeFactors();
-		System.out.println(fac.getPrimeFactors());
-			
+		factors1 = new PrimeFactorizer(100);
+		factors1.generatePrimeFactors();
+		System.out.println(factors1.getPrimeFactors());
+
 	}
 }
--- HW18/src/edu/umb/cs681/hw18/RunnablePrimeFactorizer.java
+++ HW18/src/edu/umb/cs681/hw18/RunnablePrimeFactorizer.java
@@ -4,9 +4,8 @@ import java.util.LinkedList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-
 public class RunnablePrimeFactorizer extends PrimeFactorizer implements Runnable {
-	
+
 	public RunnablePrimeFactorizer(long dividend, long from, long to) {
 		super(dividend);
 		if (from >= 2 && to >= from) {
@@ -14,56 +13,56 @@ public class RunnablePrimeFactorizer extends PrimeFactorizer implements Runnable
 			this.to = to;
 		} else {
 			throw new RuntimeException(
-				"error");
+					"error");
 		}
 	}
-	
-	protected boolean isEven(long n){
-		if(n%2 == 0){ return true; }
-		else{ return false; }
+
+	protected boolean isEven(long n) {
+		if (n % 2 == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	public void generatePrimeFactors() {
 		long divisor = from;
-	    while( dividend != 1 && divisor <= to ){
-		    if(dividend % divisor == 0) {
-		        factors.add(divisor);
-		        dividend /= divisor;
-		    }else {
-		    	if(divisor==2){ divisor++; }
-		    	else{ divisor += 2; }
-		    }
+		while (dividend != 1 && divisor <= to) {
+			if (dividend % divisor == 0) {
+				factors.add(divisor);
+				dividend /= divisor;
+			} else {
+				if (divisor == 2) {
+					divisor++;
+				} else {
+					divisor += 2;
+				}
+			}
 		}
 	}
-	
+
 	public void run() {
 		generatePrimeFactors();
 	}
 
 	public static void main(String[] args) {
-		
-		LinkedList<RunnablePrimeFactorizer> runnables = new LinkedList<RunnablePrimeFactorizer>();
 
+		LinkedList<RunnablePrimeFactorizer> runnables1 = new LinkedList<RunnablePrimeFactorizer>();
 
-		runnables.add( new RunnablePrimeFactorizer(808, 2, (long)Math.sqrt(808)/2 ));
-		runnables.add( new RunnablePrimeFactorizer(808, 1+(long)Math.sqrt(808)/2, (long)Math.sqrt(808) ));
-		
+		runnables1.add(new RunnablePrimeFactorizer(808, 2, (long) Math.sqrt(808) / 2));
+		runnables1.add(new RunnablePrimeFactorizer(808, 1 + (long) Math.sqrt(808) / 2, (long) Math.sqrt(808)));
+
 		ExecutorService executor = Executors.newFixedThreadPool(2);
 
-		executor.execute(runnables.get(0));
-		
-		executor.execute(runnables.get(1));
-		
+		executor.execute(runnables1.get(0));
+
+		executor.execute(runnables1.get(1));
+
 		executor.shutdown();
-		
 
-		LinkedList<Long> factors2 = new LinkedList<Long>();
-		runnables.forEach( (factorizer) -> factors2.addAll(factorizer.getPrimeFactors()) );
-		System.out.println("Final result for 808: " + factors2);
-		
-		
+		LinkedList<Long> factor = new LinkedList<Long>();
+		runnables1.forEach((factorizer) -> factor.addAll(factorizer.getPrimeFactors()));
+		System.out.println("Final result for 808: " + factor);
 
-		
-		
 	}
 }
